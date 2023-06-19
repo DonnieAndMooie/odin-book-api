1cfbc09852b945429fbae65ac5d211fdaa0a3bbd
Owen Eaves <owenjeaves@gmail.com>
2023-06-19 15:15:33 +0100

    Add post controller functions

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,19 +1,56 @@
-exports.post_get = (req, res) => {
+const Post = require("../models/Post");
 
+exports.post_get = async (req, res) => {
+  try {
+    const post = Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: `Could not find post with ID ${req.params.postId}` });
+  }
 };
 
-exports.posts_get = (req, res) => {
-
+exports.posts_get = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: "Could not find any posts" });
+  }
 };
 
-exports.post_post = (req, res) => {
-
+exports.post_post = async (req, res) => {
+  try {
+    const post = new Post({
+      text: req.body.text,
+      picture: req.body.picture,
+      likes: [],
+      timestamp: Date.now(),
+      author: req.user._id,
+    });
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.json({ message: "There was an error when posting" });
+  }
 };
 
-exports.post_delete = (req, res) => {
-
+exports.post_delete = async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.postId);
+    res.json(deletedPost);
+  } catch (err) {
+    res.json({ message: `Cannot delete post with id of ${req.params.postId}` });
+  }
 };
 
-exports.post_update = (req, res) => {
-
+exports.post_update = async (req, res) => {
+  try {
+    const changes = {
+      likes: req.body.likes,
+    };
+    const updatedResult = await Post.findByIdAndUpdate(req.params.postId, changes, { new: true });
+    res.json(updatedResult);
+  } catch (err) {
+    res.json({ message: `Cannot update post with the ID ${req.params.postId}` });
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
+const passport = require("passport");
 const authController = require("../controllers/authController");
 const commentController = require("../controllers/commentController");
 const postController = require("../controllers/postController");
@@ -12,30 +13,30 @@ router.post("/login", authController.login);
 
 router.post("/facebook-login", authController.login_facebook);
 
-router.get("/posts", postController.posts_get);
+router.get("/posts", passport.authenticate("jwt", { session: false }), postController.posts_get);
 
-router.get("/posts/:postId", postController.post_get);
+router.get("/posts/:postId", passport.authenticate("jwt", { session: false }), postController.post_get);
 
-router.post("/posts", postController.post_post);
+router.post("/posts", passport.authenticate("jwt", { session: false }), passport.authenticate("jwt", { session: false }), postController.post_post);
 
-router.delete("/posts", postController.post_delete);
+router.delete("/posts/:postId", passport.authenticate("jwt", { session: false }), postController.post_delete);
 
-router.put("/posts/:postId", postController.post_update);
+router.put("/posts/:postId", passport.authenticate("jwt", { session: false }), postController.post_update);
 
-router.get("/posts/:postId/comments", commentController.comments_get);
+router.get("/posts/:postId/comments", passport.authenticate("jwt", { session: false }), commentController.comments_get);
 
-router.get("/posts/:postId/comments/:commentId", commentController.comment_get);
+router.get("/posts/:postId/comments/:commentId", passport.authenticate("jwt", { session: false }), commentController.comment_get);
 
-router.post("/posts/:postId/comments", commentController.comment_post);
+router.post("/posts/:postId/comments", passport.authenticate("jwt", { session: false }), commentController.comment_post);
 
-router.delete("/posts/:postId/comments/:commentId", commentController.comment_delete);
+router.delete("/posts/:postId/comments/:commentId", passport.authenticate("jwt", { session: false }), commentController.comment_delete);
 
-router.put("/posts/:postId/comments/:commentId", commentController.comment_update);
+router.put("/posts/:postId/comments/:commentId", passport.authenticate("jwt", { session: false }), commentController.comment_update);
 
-router.get("/users", userController.users_get);
+router.get("/users", passport.authenticate("jwt", { session: false }), userController.users_get);
 
-router.get("/users/:userId", userController.user_get);
+router.get("/users/:userId", passport.authenticate("jwt", { session: false }), userController.user_get);
 
-router.put("/users/:userId", userController.user_update);
+router.put("/users/:userId", passport.authenticate("jwt", { session: false }), userController.user_update);
 
 module.exports = router;
