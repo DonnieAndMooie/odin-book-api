801118b2593ec84d28d9d5c91214405602716654
Owen Eaves <owenjeaves@gmail.com>
2023-06-22 14:36:28 +0100

    Set default profile picture

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema({
   friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   email: { type: String },
   password: { type: String },
-  picture: { type: String },
+  picture: { type: String, default: "https://i0.wp.com/researchictafrica.net/wp/wp-content/uploads/2016/10/default-profile-pic.jpg?ssl=1" },
   facebookId: { type: String },
   requests_received: [{ type: Schema.Types.ObjectId, ref: "User" }],
   requests_sent: [{ type: Schema.Types.ObjectId, ref: "User" }],
